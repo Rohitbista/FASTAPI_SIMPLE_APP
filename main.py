b8e6852f05d6f72ea55e84a9137145316ced9e36
Rohitbista <114638542+Rohitbista@users.noreply.github.com>
2025-09-02 11:21:31 +0545

    Added a list with post and get methods

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
     return {"detail": "This is a simple app to keep track of items"}
+
+@app.post("/items")
+def add_item(item: str) -> str:
+    items.append(item)
+    return f"Item {item} has been successfully added"
+
+@app.get("/items")
+def get_all_items():
+    return items
 
